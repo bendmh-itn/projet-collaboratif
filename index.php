47bc57c92a67bc6424329c818b9bfb7c7bb33f10
Baudson Béatrice <baudb@site.asty-moulin.be>
2022-10-06 11:02:20 +0200

    ajout modèle + changement extension

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site collaboratif</title>
+</head>
+<body>
+    <h1>projet collaboratif 5TTI</h1>
+    <ul>
+        <li><a href="modele/index.php" target="_blank">Modèle HTML</a></li>
+        <li>Groupe 1
+            <ul>
+                <li><a href="">Jayson</a></li>
+                <li><a href="">Guillaume</a></li>
+                <li><a href="">Andras</a></li>
+                <li><a href="">Adam</a></li>
+                <li><a href="">Elias</a></li>
+                <li><a href="">Louis</a></li>
+                <li><a href="">Romane</a></li>
+                <li><a href="">Clément</a></li>
+                <li><a href="">Nathan</a></li>
+            </ul>
+        </li>
+        <li>Groupe 2
+            <ul>
+                <li><a href="">Sefedin</a></li>
+                <li><a href="">Simon</a></li>
+                <li><a href="">Yasin</a></li>
+                <li><a href="">Alexandre</a></li>
+                <li><a href="">Aymeric</a></li>
+                <li><a href="">Victor</a></li>
+                <li><a href="">Ewan</a></li>
+                <li><a href="">Tom</a></li>
+                <li><a href="">Benjamin</a></li>
+                <li><a href="">Gaspard</a></li>
+            </ul>
+        </li>
+    </ul>
+</body>
+</html>

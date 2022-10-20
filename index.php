920e7f8180de005ebada80176ebba07fdee7efab
lo-franckenstein <220153@site.asty-moulin.be>
2022-10-20 14:33:46 +0200

    Version 1 non finie

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                 <li><a href="">Andras</a></li>
                 <li><a href="">Adam</a></li>
                 <li><a href="">Elias</a></li>
-                <li><a href="">Louis</a></li>
+                <li><a href="Louis/index.php">Louis</a></li>
                 <li><a href="">Romane</a></li>
                 <li><a href="">Clément</a></li>
                 <li><a href="">Nathan</a></li>

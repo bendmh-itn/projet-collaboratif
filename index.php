f6c74d57e90b72c5cc6b6a9bc36049564f86c910
Grégoire Alexandre <210278@CCM-Asty-Moulin.local>
2022-10-20 14:28:55 +0200

    modification du alexandre pour diriger vers l'index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
                 <li><a href="">Sefedin</a></li>
                 <li><a href="">Simon</a></li>
                 <li><a href="">Yasin</a></li>
-                <li><a href="">Alexandre</a></li>
+                <li><a href="Alexandre/index.php">Alexandre</a></li>
                 <li><a href="">Aymeric</a></li>
                 <li><a href="">Victor</a></li>
                 <li><a href="">Ewan</a></li>

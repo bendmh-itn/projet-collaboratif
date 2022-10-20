da803004eb4243950d18259bca17e8f13aff3c47
Pholien Victor <180229@CCM-Asty-Moulin.local>
2022-10-20 13:36:56 +0200

    Création du Fichier Victor + ajout du form1, ajout du dossier style.css + fichir

---
+++ Victor/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Victor_Pholien</title>
+</head>
+<body>
+    <form action="" method="">
+        <fieldset>
+            <legend>Coordonées</legend>
+            <div>
+                <label for="nom"> Entrez votre nom : </label>
+                <input name="nom" type="text" id="nom" placeholder="Votre nom" required>
+            </div>
+            <div>
+                <label for="prenom"> Entrez votre prenom : </label>
+                <input name="prenom" type="text" id="prenom" placeholder="Votre prénom" required >
+            </div>
+            <div>
+                <label for="naissance"> Date de naissance : </label>
+                <input name="naissance" type="date" id="naissance" required>
+            </div>
+            <div>
+                <label for="email">Email :</label>
+                <input name="email" type="email" id="email" placeholder="Votre email" required>
+            </div>
+            <div>
+                <label for="pays">Pays : </label>
+                <select name="pays" id="pays">
+                    <optgroup label="Europe">
+                    <option value="belgique">Belgique</option>
+                    <optgroup label="Amérique du Nord">
+                    <option value="usa">Etats-Unis</option>
+                    <option value="canada">Canada</option>
+                    <optgroup label="Asie">
+                    <option value="chine">Chine</option>
+                    <option value="japon">Japon</option>
+                </select>
+            </div>
+            <div>
+                <label for="nombre">Nombre de copies souhaitées : </label>
+                <input name="nombre" type="number" id="nombre" min="1" max="20" value="1">
+            </div>
+        </fieldset>
+        <fieldset>
+            <legend>Périodicité</legend>
+            <div>
+                <input type="radio" id="hebdomadaire" name="hebdomadaire" checked>
+                <label for="hebdomadaire">Newsletter hebdomadaire</label>
+            </div>
+            <div>
+                <input type="radio" id="mensuelle" name="mensuelle">
+                <label for="mensuelle">Newsletter mensuelle</label>
+            </div>
+            <div>
+                <input type="radio" id="trimestrielle" name="trimestrielle">
+                <label for="trimestrielle">Newsletter trimestrielle</label>
+            </div>
+        </fieldset>
+        <fieldset>
+            <legend>Version Papier - Informatique</legend>
+            <div>
+                <input type="checkbox" id="papier" name="papier" checked>
+                <label for="papier">Recevoir la version papier</label>
+            </div>
+            <div>
+                <input type="checkbox" id="info" name="info" checked>
+                <label for="info">Recevoir la version informatique</label>
+            </div>
+        </fieldset>
+        <input type="submit" value="Envoyer">
+    </form>
+</body>
+</html>

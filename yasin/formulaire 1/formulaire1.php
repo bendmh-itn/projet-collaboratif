f9e3ba8b0038015ad5d85865f5686a04666bad77
Gerimkhanov Yasin <200460@CCM-Asty-Moulin.local>
2022-11-10 13:26:08 +0100

    mis les couleurs de font et du box

--- yasin/formulaire 1/formulaire1.php
+++ yasin/formulaire 1/formulaire1.php
@@ -9,79 +9,84 @@
 </head>
 <body> 
     <form action="">
-        <fieldset>
-            <legend>vos coordonées</legend>
-            
-                <div>     
-                    <label for="">Nom</label>
-                    <input name="text" type="text" placeholder="votre nom">
-                </div>
-                </div>
-                    <label for="">Prénom</label>
-                    <input name="text" type="text" placeholder="votre nom">
-                </div>
+        <div class="flex justify-content-space-around">
+            <fieldset>
+                <legend>vos coordonées</legend>
+                    <div>     
+                        <label for="">Nom</label>
+                        <input name="text" type="text" placeholder="votre nom">
+                    </div>
+                    <div>
+                        <label for="">Prénom</label>
+                        <input name="text" type="text" placeholder="votre nom">
+                    </div>
+                    <div>
+                        <label for="">Date de naissance</label>
+                        <input name="date" type="date">
+                    </div>
+                    <div>
+                        <label for="">Entrez un mail :</label>
+                        <input type="email" id="mail" placeholder="votre adresse mail">
+                    </div>
+                    <div>
+                        <label for=""> Pays : </label>
+                        <select name="pays" id="pays">
+                            <optgroup label="Europe">
+                                <option value="belgique">Belgique</option>
+                        </optgroup>
+                            <optgroup label="amerique du nord">
+                            <option value="etats-unis">Algérie</option>
+                            <option value="canada">Tunisie</option>
+                        </optgroup>
+                        <optgroup label="asie">
+                            <option value="japon">Canada</option>
+                            <option value="chine">Canada</option>
+                        </optgroup>
+                        </select>
+                    </div>
+                    <div>
+                        <label for="">Nombre de copie souhaiter</label>
+                        <input type="nombre" id="nombre">
+                    </div>
+            </fieldset>
+        </div>
+        <div class="flex justify-content-space-around">    
+            <fieldset>
+            <legend>versions paper - informatique</legend>
                 <div>
-                    <label for="">Date de naissance</label>
-                    <input name="date" type="date">
+                    <div>
+                        <input type="checkbox" id="html" name="competences" value="html">
+                        <label for="html">recevoir la version papier</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="html" name="competences" value="html">
+                        <label for="html">recevoir la version informatique</label>
+                    </div>
                 </div>
-                <div>
-                    <label for="">Entrez un mail :</label>
-                    <input type="email" id="mail" placeholder="votre adresse mail">
-                </div>
-                <div>
-                    <label for=""> Pays : </label>
-                    <select name="pays" id="pays">
-                        <optgroup label="Europe">
-                            <option value="belgique">Belgique</option>
-                    </optgroup>
-                        <optgroup label="amerique du nord">
-                        <option value="etats-unis">Algérie</option>
-                        <option value="canada">Tunisie</option>
-                    </optgroup>
-                    <optgroup label="asie">
-                        <option value="japon">Canada</option>
-                        <option value="chine">Canada</option>
-                    </optgroup>
-                    </select>
-                </div>
-                <div>
-                    <label for="">Nombre de copie souhaiter</label>
-                    <input type="nombre" id="nombre">
-                </div>
+            </fieldset> 
+        </div>
             
-        </fieldset>
-        <fieldset>
-        <legend>periodicités</legend>
-            
-            <div>
-                <input type="radio" id="h" name="sexe" value="homme">
-                <label for="h">newsletter hebdomadaire</label>
-            </div>
-            <div>
-                <input type="radio" id="f" name="sexe" value="femme">
-                <label for="f">newsletter mensuelle</label>
-            </div>
-            <div>
-                <input type="radio" id="f" name="sexe" value="femme">
-                <label for="f">newsletter trimestrielle</label>
-            </div>
-            
-        </fieldset>  
-        <fieldset>
-        <legend>versions paper - informatique</legend>
-            
-                <div>
-                    <input type="checkbox" id="html" name="competences" value="html">
-                    <label for="html">recevoir la version papier</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="html" name="competences" value="html">
-                    <label for="html">recevoir la version informatique</label>
-                </div>
-            
-        </fieldset> 
-        
-        <input type="submit" value="Envoyer">
+        <div class="flex justify-content-space-around">
+            <fieldset>
+            <legend>periodicités</legend>
+                    <div>
+                        <input type="radio" id="h" name="sexe" value="homme">
+                        <label for="h">newsletter hebdomadaire</label>
+                    </div>
+                    <div>
+                        <input type="radio" id="f" name="sexe" value="femme">
+                        <label for="f">newsletter mensuelle</label>
+                    </div>
+                    <div>
+                        <input type="radio" id="f" name="sexe" value="femme">
+                        <label for="f">newsletter trimestrielle</label>
+                    </div>
+            </fieldset>
+             
+        </div>    
+        <div class="flex justify-content-space-around">
+            <input type="submit" value="Envoyer" > 
+        </div>
         
     </form>
 </body>
